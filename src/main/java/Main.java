5b8860b5a15ce2dea8485575a6d8ea99352bdea6
Hleb Zhuk <hleb7199@gmail.com>
2024-09-22 12:30:32 +0300

    Task done

---
+++ src/main/java/Main.java
@@ -0,0 +1,38 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+        String arr[] = {"One", "Two", "One", "Three", "Two", "Five", "One", "Five", "Three", "One"};
+        Set<String> mySet = new HashSet<>();
+        for (int i = 0; i < arr.length; i++) {
+            mySet.add(arr[i]);
+        }
+        System.out.println("Список уникальных слов " + mySet);
+
+        HashMap<String, Integer> wordsCount = new HashMap<>();
+        for (String word : arr) {
+            if (wordsCount.containsKey(word)) {
+                wordsCount.put(word, wordsCount.get(word) + 1);
+            } else {
+                wordsCount.put(word, 1);
+            }
+        }
+        System.out.println("Количество вхождений: ");
+        for (String word : wordsCount.keySet()) {
+            System.out.println(word + "-" + wordsCount.get(word));
+        }
+        // Справочник
+        System.out.println();
+        Phonebook phonebook = new Phonebook();
+        phonebook.add(912, "Dima");
+        phonebook.add(111, "Dima");
+        phonebook.add(123, "Vasia");
+
+        phonebook.get("Dima");
+        phonebook.get("Vasia");
+
+
+    }
+}
